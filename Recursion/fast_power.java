d8ea61419e5f83aad93287fcd563990843fd72d2
nasir-buddy <syedali172000@gmail.com>
2023-08-20 22:10:04 +0530

    fix some variable and input

--- Recursion/fast_power.java
+++ Recursion/fast_power.java
@@ -1,19 +1,28 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class fast_power {
-    public static double fastpower(double a, int b, int m){
-        if(b == 0){
+    public static double fastpower(double n, int x, int m){
+        if(x == 0){
             return 1;
         }
-        if(b < 0){
-            return fastpower(1 / a, Math.abs(b) ,m);
+        if(x < 0){
+            return fastpower(1 / n, Math.abs(x) ,m);
         }
-        if((b & 1) == 0){
-            return fastpower((a % m  * a % m) % m, b / 2 , m);
+        if((x & 1) == 0){
+            return fastpower((n % m  * n % m) % m, x / 2 , m);
         }
-        return (a % m * fastpower(a, b - 1 , m) % m) % m;
+        return (n % m * fastpower(n, x - 1 , m) % m) % m;
     }
     public static void main(String[] args) {
-        System.out.println(fastpower( 2 , -10, 500));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the power no: ");
+        int x = sc.nextInt();
+        System.out.print("Enter the modulo % : ");
+        int m = sc.nextInt();
+        System.out.println(fastpower(n, x, m));
     }
 }
