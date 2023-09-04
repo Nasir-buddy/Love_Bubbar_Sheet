2a9b223a2ffcaec910a76fde78a179bab93b0365
nasir-buddy <syedali172000@gmail.com>
2023-09-04 22:11:23 +0530

    Generate brackets

---
+++ Backtracking/Generate_Brackets.java
@@ -0,0 +1,39 @@
+package Backtracking;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Generate_Brackets {
+    public static List<String> generateBrackets(int n){
+        List<String> ans = new ArrayList<>();
+        helpergenerate(ans, "", n, 0, 0, 0);
+        return ans;
+    }
+    public static void helpergenerate(List<String> ans, String current, int n, int i, int open , int close){
+            if(i == 2 * n){
+                ans.add(current);
+                return;
+            }
+            if(open < n){
+                current = current + '(';
+                helpergenerate(ans, current, n, i + 1, open + 1, close);
+                current = current.substring(0 , current.length() - 1);
+            }
+            if(close < open){
+                current = current + ')';
+                helpergenerate(ans, current, n, i + 1, open, close + 1);
+            }
+        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // List<String> pass = new ArrayList<>(n);
+        // for(int i = 0; i < n; i++){
+        //     String put = sc.next();
+        //     pass.add(put);
+        // }
+        // System.out.println(pass);
+        List<String> ans = new ArrayList<>(generateBrackets(n)); 
+        System.out.println(ans);
+    }
+}
